5952c82d7b15e661df9972222424132608255f4c
Philippe Lemaire <lemaire.philippe@gmail.com>
2021-03-23 12:11:46 +0100

    implemented first rolls

--- ryuutama.py
+++ ryuutama.py
@@ -3,32 +3,15 @@ import random
 
 
 class PlayerCharacter:
-    def __init__(self):
-        self.name = pyip.inputStr("What is your character's name?\n")
 
-    ClassList = sorted(['Minstrel', 'Merchant', 'Farmer', 'Hunter', 'Healer',
-                       'Noble', 'Artisan'])
 
-    typeList = sorted(['Attack', 'Technical', 'Magical'])
-    genderList = (['girl', 'boy', 'non-gendered', 'other'])
-
-    Class = pyip.inputMenu(ClassList, lettered=True,
-                           prompt='Select your class…\n')
-
-    type = pyip.inputMenu(typeList, lettered=True, prompt='Select a type\n')
-
-    level = 1
-    XP = 0
-    gender = pyip.inputMenu(genderList, lettered=True, prompt='You are a…\n')
 
     def __repr__(self):
-        return f'''{self.name} is a level {self.level} {self.Class}
-    of the {self.type} Type.'''
-
-
-    
+        return f'{self.name} is a level {self.level} {self.Class} of the {self.type} Type.'
+   
 
     def setStartingAbilityScores(self):
+        print("\nLet's set ability scores for", self.name,'\n')
         self.abilityScores = {'STR': 6, 'DEX': 6, 'INT': 6, 'SPI': 6}
         setChoices = ['Average Set, 6 in all ability scores',
                       'Standard Set: 4 6 6 8',
@@ -48,20 +31,53 @@ class PlayerCharacter:
             self.abilityScores[scoreToDump2] -= 2
             for score in scoresToBump:
                 self.abilityScores[score] += 2
+        
+        self.maxHP = self.abilityScores['STR'] * 2
+        self.maxSP = self.abilityScores['SPI'] * 2
+        
+        print("Your ability scores are:", self.abilityScores)
 
 
+    def __init__(self):
+        print("Let's create a character for you…\n")
+        ClassList = sorted(['Minstrel', 'Merchant', 'Farmer', 'Hunter', 'Healer',
+                       'Noble', 'Artisan'])
 
+        typeList = sorted(['Attack', 'Technical', 'Magical'])
+    
+        genderList = (['girl', 'boy', 'non-gendered', 'other'])
+
+        self.name = pyip.inputStr("What is your character's name?\n")
+        self.Class = pyip.inputMenu(ClassList, lettered=True,
+                           prompt='Select your class…\n')
+
+        self.type = pyip.inputMenu(typeList, lettered=True, prompt='Select a type\n')
+
+        self.level = 1
+        self.XP = 0
+        self.gender = pyip.inputMenu(genderList, lettered=True, prompt='You are a…\n')
+
+        self.fumbles = 0
+        self.condition = None
+    
+
+    def rollTest(self, ability1, ability2, modifier=0):
+        roll = [random.randint(1, self.abilityScores[ability1]), random.randint(1, self.abilityScores[ability2]), modifier]
+        if sum(roll[:-1]) == 2:
+            print('Oh no, a fumble!')
+            self.fumbles += 1
+        elif roll[:-1] == [self.abilityScores[ability1], self.abilityScores[ability2]] or roll[:-1] == [6, 6]:
+            print('CRITICAL SUCCESS!')
+        else:
+            print(f"You rolled a {sum(roll)}.")
+    
+    def rollCondition(self, modifier=0):
+        return self.rollTest('STR', 'SPI', modifier)
+            
 
 class Ryuujin:
     pass
 
 
-phil = PlayerCharacter()
 
-print(phil)
-phil.setStartingAbilityScores()
 
-print(f"{phil.name}'s strength is", phil.abilityScores['STR'])
-print(f"{phil.name}'s dexterity is", phil.abilityScores['DEX'])
-print(f"{phil.name}'s intellect is", phil.abilityScores['INT'])
-print(f"{phil.name}'s spirit is", phil.abilityScores['SPI'])
